2189ede570fe4659085d91ec2e4920a2e46bcd13
Ashish Jain <ashish2103parmar@gmail.com>
2021-06-02 10:18:46 +0530

    file name change

---
+++ serivce.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Service interface {
+	Start()
+}
+
+type Greeter struct {
+	msg string
+}
+
+func (g *Greeter) Start() {
+	g.msg = "Hello, World!"
+}
+
+func main() {
+	var service Service = &Greeter{}
+	service.Start()
+	greeter := service.(*Greeter)
+	fmt.Println(greeter.msg)
+}
